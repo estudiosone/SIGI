ea3f99d7bd3387a6e24f3a423afcff923121b633
Diego Rodríguez <drodriguez@estudios.one>
2019-02-15 11:21:50 -0300

    Modificaciones varias

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+const path = require('path')
+
 module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        includePaths: [path.resolve(__dirname, './node_modules')]
+      }
+    }
+  },
   pwa: {
     workboxPluginMode: 'InjectManifest',
     name: 'SIGI',
